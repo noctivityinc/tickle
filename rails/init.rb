ce78d880cb881890393a9acef01cb644e46d47f4
Joshua Lippiner <jlippiner@noctivity.com>
2010-05-05 17:10:28 -0400

    added generator folder

--- rails/init.rb
+++ rails/init.rb
@@ -1,9 +1,8 @@
 require 'active_record'
-require 'active_view'
 
 require 'tickle'
 require 'acts_as_ticklish/acts_as_ticklish'
-require 'acts_as_ticklish/model/tickle'
+require 'acts_as_ticklish/models/tickle'
 
 if defined?(ActiveRecord::Base)
   ActiveRecord::Base.send :include, Tickle::ActsAsTicklish
